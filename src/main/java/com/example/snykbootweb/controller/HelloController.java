11ec5c2fcf6c474ce25007d17e741e4f7e72a11b
papicella <pas.apicella@gmail.com>
2024-03-15 17:25:54 +1100

    Added some setup instructions to the repo

--- src/main/java/com/example/snykbootweb/controller/HelloController.java
+++ src/main/java/com/example/snykbootweb/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.snykbootweb.controller;
 
+import com.example.snykbootweb.DatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    DatabaseService databaseService;
+
     @GetMapping ("/")
     public String sayHello () {
+        String password = databaseService.getPassword();
         return "hello world web app for snyk springboot jdbc demo";
     }
 }
